45cecfac5fda1a47067708a1c8a5c8cc1fe5047b
Dylan DiGeronimo <ddigeron@stevens.edu>
2018-09-05 14:17:13 -0400

    Started adding to files

---
+++ hw1/AssignmentOne.java
@@ -0,0 +1,17 @@
+/*
+ * CS 511 HW 1
+ * Dylan DiGeronimo and Ryan Locke
+ * I pledge my honor that I have abided by the Stevens Honor System
+ */
+
+public class AssignmentOne {
+
+    public static List<Integer> lprimes(List<Integer[]> intervals) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
+
---
+++ hw1/PrimeFinder.java
@@ -0,0 +1,24 @@
+public class PrimeFinder implements Runnable {
+
+    private Integer start;
+    private Integer end;
+    private List<Integer> primes;
+
+    PrimeFinder(Integer startNum, Integer endNum) {
+        // Constructs a PrimeFinder
+    }
+
+    public List<Integer> getPrimesList() {
+        // Returns the value of the attribute primes
+    }
+
+    public Boolean isPrime(int n) {
+        // Determines whether its argument is prime or not
+    }
+
+    public void run() {
+        // Adds all primes in [this.start, this.end] to the attribute primes
+    }
+
+
+}
