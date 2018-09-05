407d1c5ab2e72800709357203a8a7913d2aa2552
Dylan DiGeronimo <ddigeron@stevens.edu>
2018-09-05 14:48:06 -0400

    Added more work --> Doesn't compile

--- hw1/AssignmentOne.java
+++ hw1/AssignmentOne.java
@@ -4,14 +4,33 @@
  * I pledge my honor that I have abided by the Stevens Honor System
  */
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class AssignmentOne {
 
-    public static List<Integer> lprimes(List<Integer[]> intervals) {
-        // TODO
-    }
+    // PrimeFinder pf = new PrimeFinder();
+
+    // public static List<Integer> lprimes(List<Integer[]> intervals) {
+    //     // TODO
+    // }
 
     public static void main(String[] args) {
+        // Parse command line args
+        int val1;
+        int val2;
+        int[] twoInts = new int[2];
+        List<Integer> listOfArrays = new ArrayList<Integer>();
+        for (int i = 0; i < args.length-1; i++) {
+            val1 = Integer.parseInt(args[i]);
+            val2 = Integer.parseInt(args[i+1]);
+            twoInts[0] = val1;
+            twoInts[1] = val2;
+            listOfArrays.add(twoInts);
+        }
+        // for (int i = 0; i < listOfArrays.size(); i++) {
+        //     System.out.println(listOfArrays[i]);
+        // }
     }
-
 }
 
