a1fc34a904f94bcfd6fbc1b8797fbec56a2eef39
Dylan DiGeronimo <ddigeron@stevens.edu>
2018-09-19 20:48:47 -0400

    I hate the gym

--- Assignment2/Gym.java
+++ Assignment2/Gym.java
@@ -7,6 +7,7 @@
 package Assignment2;
 
 import java.util.EnumMap;
+import java.util.concurrent;
 
 public class Gym {
 
@@ -15,17 +16,35 @@ public class Gym {
     private static final in GYM_REGISTERED_CLIENTS = 10000;
 
     private Map<WeightPlateSize,int> noOfWeightPlates = new EnumMap<WeightPlateSize,int>(WeightPlateSize.class);
-    noOfWeightPlates.put(GFG.SMALL_3KG, 110);
-    noOfWeightPlates.put(GFG.MEDIUM_5KG, 90);
-    noOfWeightPlates.put(GFG.LARGE_10KG, 75);
+    noOfWeightPlates.put(WeightPlateSize.SMALL_3KG, 110);
+    noOfWeightPlates.put(WeightPlateSize.MEDIUM_5KG, 90);
+    noOfWeightPlates.put(WeightPlateSize.LARGE_10KG, 75);
 
-    private Set<Integer> clients; // For generating fresh client ids
+    // For generating fresh client ids
+    private Set<Integer> clients; 
 
-    private ExecutorService executor;
+    // Thread Executor Service
+    private ExecutorService executor = Executors.newFixedThreadPool(GYM_SIZE);
 
-    // TODO: More Semaphores?
-    Semaphore numApparatus = new Semaphore(0); // TODO: Should this be 0? 
-   
+    // Semaphores
+    // One for access to each plate size
+    public Semaphore grabSmall = new Semaphore(1);
+    public Semaphore grabMedium = new Semaphore(1);
+    public Semaphore grabLarge = new Semaphore(1);
+    // One for the amount of each plate size
+    public Semaphore amtSmall = new Semaphore(noOfWeightPlates.get(WeightPlateSize.SMALL_3KG));
+    public Semaphore amtMedium = new Semaphore(noOfWeightPlates.get(WeightPlateSize.MEDIUM_5KG));
+    public Semaphore amtLarge = new Semaphore(noOfWeightPlates.get(WeightPlateSize.LARGE_10KG));
+    // One for each machine
+    public Semaphore legPress = new Semaphore(5);
+    public Semaphore barbell = new Semaphore(5);
+    public Semaphore hackSquat = new Semaphore(5);
+    public Semaphore legExtension = new Semaphore(5);
+    public Semaphore legCurl = new Semaphore(5);
+    public Semaphore latPull = new Semaphore(5);
+    public Semaphore pecDeck = new Semaphore(5);
+    public Semaphore cableCross = new Semaphore(5);
+    
     public void run() {
 
     }
