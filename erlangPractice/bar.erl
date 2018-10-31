8624a4e1b548a144cdf5d86d7fb146c64ed76f67
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2018-10-31 11:20:38 -0400

    Completed bar problem, adding possibility for process to change once enough time has passed

--- erlangPractice/bar.erl
+++ erlangPractice/bar.erl
@@ -6,7 +6,17 @@
 start(W, M) ->
     S=spawn(?MODULE, server, [0,0]),
     [spawn(?MODULE, woman, [S]) || _ <- lists:seq(1, W)],
-    [spawn(?MODULE, man, [S]) || _ <- lists:seq(1, M)].
+    [spawn(?MODULE, man, [S]) || _ <- lists:seq(1, M)],
+    [spawn(?MODULE, itGotLate, [1000,S])].
+
+itGotLate(Time, S) ->
+    timer:sleep(Time),
+    Ref = make_ref(),
+    S ! {self(), Ref, itGotLate},
+    receive
+	{S, Ref, ok} ->
+	    done
+    end.
 
 woman(S) -> % Reference to Pid of server
     S ! {self(), woman}.
@@ -19,13 +29,35 @@ man(S) -> % Reference to Pid of server
 	    ok
     end.
 
-server(Women) -> % "Counter" of Women
+answer_pending_men_requests() ->
+    receive
+	{From,Ref,man} ->
+	    From!{self(),Ref,ok},
+	    answer_pending_men_requests()
+    after 0 ->
+	    ok
+end.
+
+server(Women, false) -> % It is not late
     receive
 	{From, woman} ->
 	    From ! {self(), ok},
-	    server(Women+1);
+	    server(Women+1, false);
 	{From, Ref, man} when Women > 1 ->
 	    From ! {self(), Ref, ok},
-	    server(Women-2)
+	    server(Women-2, false);
+	{From, Ref, itGotLate} ->
+	    From ! {self(), Ref, ok}, %% reply to the itGotLate thread
+	    answer_pending_men_requests(),
+	    server(true)
+    end.
+
+server(true) -> % It is late
+    receive
+	{_From, woman} ->
+	    server(true);
+	{From, Ref, man} ->
+	    From ! {self(), Ref, ok},
+	    server(true)
     end.
     
