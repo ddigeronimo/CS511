22f143bbe9a26d21bafef67dfc1c25969e335732
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2018-11-01 08:26:47 -0400

    Added work on hw4

--- hw4/sensor.erl
+++ hw4/sensor.erl
@@ -1,2 +1,23 @@
+%% Models a sensor process
 -module(sensor).
+-author("Dylan DiGeronimo and Ryan Locke")
 -compile(export_all).
+
+sensor(ID, Watcher) ->
+    Ref = make_ref(),
+    Measurement = rand:uniform(11),
+    if Measurement == 11 ->
+	    % report "anomalous_reading"
+	    % Watcher needs to be replaced by the Pid of the sensor's watcher
+	    Watcher ! {self(), Ref, "anamalous_reading"},
+	    exit(sensor); % Should be changed to catch, but this works for now
+       Measurement < 11 ->
+	    % report to watcher
+	    % Watcher needs to be replaced by the Pid of the sensor's watcher
+	    % Check if the below tuple is correct
+	    Watcher ! {self(), Ref, {ID, Measurement}},
+	    Sleep_time = rand:uniform(10000),
+	    timer:sleep(Sleep_time)
+	    % How do we recurse?
+    end.
+
--- hw4/watcher.erl
+++ hw4/watcher.erl
@@ -1,2 +1,20 @@
+%% Models watcher processes
 -module(watcher).
+-author("Dylan DiGeronimo and Ryan Locke")
 -compile(export_all).
+
+start() ->
+    {ok, [N]} = io:fread("enter number of sensors> ", "~d"),
+    if N =< 1 ->
+	    io:fwrite("setup: range must be at least 2~n", []) ;
+       true ->
+	    Num_watchers = 1 + (N div 10),
+	    setup_loop(N, Num_watchers)
+end.
+
+% Creates N sensors and Num_watchers watchers 
+setup_loop(N, Num_watchers) ->
+    % Should I spawn all of the processes through recursion?
+    error(notImplemented).
+
+ 
