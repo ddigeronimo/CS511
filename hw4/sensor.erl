040b5dbc11ccabe8def3225fc0c312011b2f608e
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2018-11-06 20:59:40 -0500

    Finished sensor, added a readme, added more in-class exercises

--- hw4/sensor.erl
+++ hw4/sensor.erl
@@ -4,20 +4,16 @@
 -compile(export_all).
 
 sensor(ID, Watcher) ->
-    Ref = make_ref(),
     Measurement = rand:uniform(11),
     if Measurement == 11 ->
-	    % report "anomalous_reading"
-	    % Watcher needs to be replaced by the Pid of the sensor's watcher
-	    Watcher ! {self(), Ref, "anamalous_reading"},
-	    exit(sensor); % Should be changed to catch, but this works for now
+	    % report "anomalous_reading" to watcher
+	    Watcher ! {ID, "anomalous_reading"},
+	    exit(anomalous_reading);
        Measurement < 11 ->
-	    % report to watcher
-	    % Watcher needs to be replaced by the Pid of the sensor's watcher
-	    % Check if the below tuple is correct
-	    Watcher ! {self(), Ref, {ID, Measurement}},
-	    Sleep_time = rand:uniform(10000),
-	    timer:sleep(Sleep_time)
-	    % How do we recurse?
-    end.
-
+	    % report measurement to watcher
+	    Watcher ! {ID, Measurement}
+		end, 
+    Sleep_time = rand:uniform(10000),
+    timer:sleep(Sleep_time),
+    sensor(ID, Watcher).
+	  
